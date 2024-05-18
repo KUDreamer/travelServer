0e1af85decc8d91e9fbf2bfc506f70d367f449dd
lky3004me <lky3004me@gmail.com>
2024-05-18 12:08:02 +0900

    refresh config

--- src/main/java/com/example/travelServer/config/AppConfig.java
+++ src/main/java/com/example/travelServer/config/AppConfig.java
@@ -1,12 +1,18 @@
 package com.example.travelServer.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class AppConfig {
+    @Value("${google.api.key}")
+    private String apiKey;
 
+    public String getApiKey() {
+        return apiKey;
+    }
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
--- src/main/java/com/example/travelServer/controller/ApiController.java
+++ src/main/java/com/example/travelServer/controller/ApiController.java
@@ -1,9 +1,11 @@
 package com.example.travelServer.controller;
 
+import com.example.travelServer.config.AppConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.http.ResponseEntity;
@@ -15,16 +17,18 @@ public class ApiController {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private AppConfig appConfig;
+
     private static final String API_URL = "https://maps.googleapis.com/maps/api/place/details/json";
 
-    @Value("${google.api.key}")
-    private String API_KEY;
-    private static String PLACE_ID = "ChIJN1t_tDeuEmsRUsoyG83frY4";
-
     @GetMapping("/giveInfo")
-    public ResponseEntity<String> getInfo() {
-        String url = String.format("%s?fields=name,rating,formatted_phone_number&place_id=%s&key=%s",
-                API_URL, PLACE_ID, API_KEY);
+    public ResponseEntity<String> getInfo(@RequestParam String PLACE_ID) {
+
+        String fields = "name,rating,formatted_phone_number,address_components,photos,price_level,opening_hours,secondary_opening_hours";
+        String api_key = appConfig.getApiKey();
+        String url = String.format("%s?fields=%s&place_id=%s&key=%s",
+                API_URL,fields,PLACE_ID, api_key);
 
         // 외부 API 호출
         String result = restTemplate.getForObject(url, String.class);
